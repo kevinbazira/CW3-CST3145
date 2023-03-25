3c33cc4578a2fa9b04f1e6b48476f7d7b4eeb5d8
kevinbazira <kvnbzr@gmail.com>
2023-03-25 06:30:39 +0400

    remove lessons html from parent since Lesson child component has replaced it

--- src/App.vue
+++ src/App.vue
@@ -72,40 +72,13 @@
                             </div>
                         </div>
 
-                        <!--Vue Lesson child component-->
+                        <!-- Vue Lesson child component -->
                         <Lesson
                             :lessons="(searchedLessons.length === 0 ? sortedLessons : searchedLessons)"
                             :cartCount="cartCount"
                             :canAddToCart="canAddToCart"
                             @addItemToCart="addToCart"
                         />
-
-                        <!--
-                        <div class="row">
-                          <div class="col-lg-4 col-md-6 col-sm-12 pb-1" v-for="lesson in (searchedLessons.length === 0 ? sortedLessons : searchedLessons)" :key="lesson.id">
-                                <div class="card product-item border-0 mb-4">
-                                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                        <img class="img-fluid w-100" v-bind:src="lesson.images" v-bind:alt="lesson.subject">
-                                    </div>
-                                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                        <h6 class="text-truncate mb-3">{{lesson.subject}} | {{lesson.location}}</h6>
-                                        <div class="d-flex justify-content-center">
-                                            <h6>${{lesson.price}}</h6><h6 class="text-muted ml-2"></h6>
-                                        </div>
-                                        <span>Spaces: {{lesson.spaces - cartCount(lesson.id)}}</span>
-                                    </div>
-                                    <div class="card-footer bg-light border text-center">
-                                        <button class="btn btn-sm text-dark p-0 w-100 h-100" v-on:click="addToCart(lesson)" v-bind:disabled="!canAddToCart(lesson)">
-                                                <i class="fas fa-shopping-cart text-primary mr-1"></i>
-                                                Add To Cart
-                                        </button>
-                                    </div>
-                                </div>
-                          </div>
-                        </div>
-                      -->
-                        
-                      
 
                     </div>
                 </div>
