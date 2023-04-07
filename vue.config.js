0a1162038ccf261714f8296dba932a1e3fce71c3
kevinbazira <kvnbzr@gmail.com>
2023-04-07 09:02:22 +0400

    fixed prod directory

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/CW3-CST3145/'
+    : '/'
 })
